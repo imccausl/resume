3c99fc7f487302859bcc2f7ae71a6b4cbf567ec2
imccausl <imccausl@icloud.com>
2016-10-28 18:42:29 -0400

    created the infrastructure to load my resume JSON data

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,45 @@
-console.log('\'Allo \'Allo!');
+(function(document) {
+	// load resume data from JSON file
+	function get(path) {
+		return new Promise(function(resolve, reject) {
+			var req = new XMLHttpRequest();
+			req.open("GET", path);
+			req.onload = function() {
+				if (req.status === 200) {
+					// it worked
+					resolve(req.response);
+				} else {
+					// it failed
+					console.log(req.response)
+					reject(Error(req.statusText));
+				};
+			};
+		
+			req.onerror = function() {
+				// it failed with a network error
+				reject(Error("Network error.");
+			};
+		
+			req.send();
+		});
+	}
+	
+	function getJSON(data) {
+		return JSON.parse(data);
+	}
+	
+	function loadResumeData() {
+		var resumeData = {};
+		
+		get("../resume.json")
+			.then(function(response) {
+				resumeData = getJSON(response);
+				console.log(resumeData);
+			})
+			.catch(function(statusText) {
+				console.log(statusText);
+			});
+	}
+	
+	loadResumeData();
+})(document);
