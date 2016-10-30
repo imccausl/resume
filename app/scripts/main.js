8ced1bb511b68ca8b4eb5168ca19babd385e1c9a
imccausl <imccausl@icloud.com>
2016-10-30 13:14:21 -0400

    Code to load data into header template and display it

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,9 @@
 (function(document) {
 	var model = {
-		resume: {}
+		resume: {},
+		header: {},
+		
+		// methods
 		get: function(path) {
 			return new Promise(function(resolve, reject) {
 			var req = new XMLHttpRequest();
@@ -26,16 +29,53 @@
 		}
 	};
 	
+	var view = {
+		init: function() {
+			// compile the handlebars templates
+			console.log("Initializing the view!");
+			console.log(model.resume.info);
+			
+			app.buildHeader(model.resume.info);
+			view.render()
+			
+		},
+		
+		render: function() {
+			
+		}
+	};
+	
 	var app = { 
 		init: function() { 
+			// load the JSON data for the resume
 			model.get("./resume.json")
 			.then(function(response) {
+				// load was successful. Parse the JSON data
 				model.resume = JSON.parse(response);
 				console.log(model.resume);
+			}).then(function(response) {
+				// parsing was successful start initializing the view
+				view.init();
 			})
 			.catch(function(status) {
 				console.log(status);
 			});
+		},
+		
+		buildHeader: function(headerData) {
+			var source, template, placement;
+			
+			console.log(headerData.socialFeed);
+			
+			source = $('#r-header').html();
+			template = Handlebars.compile(source);
+			model.header = template(headerData);
+			
+			$('#r-header-view').html(model.header)
+		},
+		
+		compileTemplates: function() {
+			
 		}
 	};
 	
